d51246df34093b57ab904d51fc8e38b91fe5cb36
Amir Jamshidi <aabbccddee9898@gmail.com>
2023-11-16 17:46:53 +0330

    ***Start Working On Project***

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 function App() {
-  return (
-    <>
-      <span className="bg-red-600">new</span>
-      <p className="bg-red-500">TEST</p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
