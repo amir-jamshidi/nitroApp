9ba3223043ef725230fa56d8fcc40cca561604f9
Dev-Amir <143460956+Dev-Amir@users.noreply.github.com>
2023-11-16 17:17:39 +0330

    **Create Base Structure**

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
+
+    extend: {
+      fontFamily: {
+        'dana': 'dana',
+        'dana-medium': 'dana-medium',
+        'dana-bold': 'dana-bold',
+        'morabba': 'morabba',
+        'morabba-medium': 'morabba-medium',
+        'morabba-bold': 'morabba-bold',
+      },
+      container: {
+        center: true,
+        padding: '12px'
+      }
+    },
   },
   plugins: [],
 }
