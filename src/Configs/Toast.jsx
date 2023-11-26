3d385eb4f56ccdd24a51213c0c45313e5d4c8417
Amir Hossein Jamshidi <48350287+amir-jamshidi@users.noreply.github.com>
2023-11-26 16:40:23 +0330

    Feat : Create Warning Fn Toast

--- src/Configs/Toast.jsx
+++ src/Configs/Toast.jsx
@@ -25,3 +25,15 @@ export const showToastError = (title) => {
     theme: "dark",
   });
 };
+export const showToastWarning = (title) => {
+  toast.warning(title, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
