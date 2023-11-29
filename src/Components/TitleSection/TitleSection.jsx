c37c4fc3b26aa83b3c642f5dec3af2897da631dd
Amir Hossein Jamshidi <48350287+amir-jamshidi@users.noreply.github.com>
2023-11-29 15:10:39 +0330

    Fix : TitleSection File

--- src/Components/TitleSection/TitleSection.jsx
+++ src/Components/TitleSection/TitleSection.jsx
@@ -1,9 +1,7 @@
 const TitleSection = ({ title }) => {
   return (
     <div className="border-b border-b-black/5 dark:border-b-white/5 pb-2">
-      <p className="font-morabba-medium text-center text text-slate-800 dark:text-slate-200  text-lg">
-        {title}
-      </p>
+      <p className="font-morabba-medium text-center text text-slate-800 dark:text-slate-200  text-lg">{title}</p>
     </div>
   );
 };
