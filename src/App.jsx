aeb1230c687c7164183df31c2617c8636112123b
Amir Hossein Jamshidi <48350287+amir-jamshidi@users.noreply.github.com>
2024-07-19 22:51:40 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { getMe } from "./Redux/Reducers/authInfos";
 function App() {
   
 
+
   const authInfos = useSelector((state) => state.authInfos);
   const routerApplication = useRoutes(routes);
   const dispatch = useDispatch();
